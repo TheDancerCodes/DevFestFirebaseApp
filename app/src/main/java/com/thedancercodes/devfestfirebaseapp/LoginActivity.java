aca44d8d7d5b80bfeaf1682c89238cc78ba45ea7
Roger Taracha <rojtaracha@gmail.com>
2017-11-15 10:53:02 +0300

    [CHORE] TODOs for Login Activity.

---
+++ app/src/main/java/com/thedancercodes/devfestfirebaseapp/LoginActivity.java
@@ -0,0 +1,48 @@
+package com.thedancercodes.devfestfirebaseapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by @thedancercodes on 15/11/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    // TODO: Declare variables
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // TODO: Get FireBase auth instance
+
+
+        // TODO: A condition for when a user is already logged in.
+
+
+        // Set the Content View
+        setContentView(R.layout.activity_login);
+
+        // Add the toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        // TODO: Inflate View elements in the LoginActivity
+
+
+        // TODO: Get FireBase auth instance
+
+
+        // TODO: Sign Up OnClick Listener
+
+
+        // TODO: Password Reset OnClick Listener
+
+
+        // TODO: Login OnClick Listener
+
+
+    }
+}
