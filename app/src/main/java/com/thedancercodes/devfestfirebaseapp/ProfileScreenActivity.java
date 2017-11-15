2f5a96644314b8e413e4b924ecef9818d44f71b5
Roger Taracha <rojtaracha@gmail.com>
2017-11-15 10:41:26 +0300

    [CHORE] Add TODOs for Project Dependecy Set Up PT3. [+] Update Manifest File. [+] Rename MainActivity to ProfileScreenActivity

--- app/src/main/java/com/thedancercodes/devfestfirebaseapp/ProfileScreenActivity.java
+++ app/src/main/java/com/thedancercodes/devfestfirebaseapp/ProfileScreenActivity.java
@@ -3,7 +3,7 @@ package com.thedancercodes.devfestfirebaseapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ProfileScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
